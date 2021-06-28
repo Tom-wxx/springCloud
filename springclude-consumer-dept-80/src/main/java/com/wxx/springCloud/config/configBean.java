55e8264c24cbaf2c557194173741196ec9923f4c
wxx <1301275402@qq.com>
2021-06-28 13:06:19 +0800

    2021/6/23：添加集群报错修改

--- springclude-consumer-dept-80/src/main/java/com/wxx/springCloud/config/configBean.java
+++ springclude-consumer-dept-80/src/main/java/com/wxx/springCloud/config/configBean.java
@@ -1,6 +1,7 @@
 package com.wxx.springCloud.config;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class configBean {
     // 註冊到spring中
+
+    @LoadBalanced //配置负载均衡实现RestTemplate
     @Bean
     public RestTemplate getRestTemplate(){
         return  new RestTemplate();
--- springclude-consumer-dept-80/src/main/java/com/wxx/springCloud/controller/DeptConsumerController.java
+++ springclude-consumer-dept-80/src/main/java/com/wxx/springCloud/controller/DeptConsumerController.java
@@ -20,7 +20,9 @@ public class DeptConsumerController {
     @Autowired
     public RestTemplate restTemplate;  //提供多种便捷访问远程http服务的方法，简单的restful模板
 
-    private static final String REST_URL_PREFIX="http://localhost:8001";
+    //Ribbon:我们这里的地址，应该是一个变量，通过服务名来访问
+   //private static final String REST_URL_PREFIX="http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
 
     @RequestMapping("/consumer/dept/add")
     public Boolean add(Dept dept){
