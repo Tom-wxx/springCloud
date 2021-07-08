f27a18486590bf3781ec548a7c75eae3ea552b44
wxx <1301275402@qq.com>
2021-07-08 10:23:43 +0800

    2021/6/23：重新添加

--- springcloud-api/src/main/java/com/wxx/springCloud/service/DeptClientService.java
+++ springcloud-api/src/main/java/com/wxx/springCloud/service/DeptClientService.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-
 // @FeignClient:微服务客户端注解,value:指定微服务的名字,这样就可以使Feign客户端直接找到对应的微服务   微服务name
 // 切记不要加这个注解，不然会出现404访问不到
 //@Component
-@FeignClient(value = "SPRINGCLOUD-PROVIDER-DEPT",fallbackFactory = DeptClientServiceFallBackFactory.class) //allbackFactory指定降级配置类
+@FeignClient(value = "SPRINGCLOUD-PROVIDER-DEPT",fallbackFactory = DeptClientServiceFallBackFactory.class)//fallbackFactory指定降级配置类
 public interface  DeptClientService {
 
     @GetMapping("/dept/get/{id}")
